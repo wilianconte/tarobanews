e44e071d26e760acac6e61adbec8c2fd1dc17c21
Dirceu Wilian de Camargo Conte <dirceu.wilian@massanews.com>
2017-10-18 18:12:27 -0200

    Fix

---
+++ TarobaNews.Portal/Controllers/EditorialController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TarobaNews.Portal.Controllers
+{
+  public class EditorialController : Controller
+  {
+    [Route("noticias")]
+    public IActionResult Noticias()
+    {
+      return View("Index");
+    }
+  }
+}
