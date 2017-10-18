3a000fee24636a07dbfc723a8d0bb58e43627704
Dirceu Wilian de Camargo Conte <dirceu.wilian@massanews.com>
2017-10-18 18:39:51 -0200

    Fix

---
+++ TarobaNews.Portal/Controllers/BlogsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TarobaNews.Portal.Controllers
+{
+  public class BlogsController : Controller
+  {
+    [Route("blogs")]
+    public IActionResult Blogs()
+    {
+      return View("Index");
+    }
+  }
+}
